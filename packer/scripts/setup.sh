d7ab82700481f753c24c66c41a274ee491546930
Edmund Huber <me@ehuber.info>
2026-04-30 23:36:59 -0700

    enable the hw uart so that serial TTY works

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -10,6 +10,9 @@ touch /boot/firmware/ssh
 # credentials: user / password
 echo 'user:$6$0OB1OQvdH39TqH57$vtc6JKQ9kYMH0AjCpEb8xW0ptZnuiTdejcoAeqWpCMIrV9ICuS7mm9YU0zAKzRzcEom/RIrr37iAkYKdxTGTG.' > /boot/firmware/userconf.txt
 
+# Route the hardware UART to the GPIO pins (40-pin header, pins 8/10)
+grep -qxF 'enable_uart=1' /boot/firmware/config.txt || echo 'enable_uart=1' >> /boot/firmware/config.txt
+
 apt-get update
 apt-get install -y --no-install-recommends \
     python3
